04dc9f8188dc66d3a7fe63b17614b448a2c9754e
Sourarghya De <cyborgninjaraiden62@gmail.com>
2025-06-21 13:02:08 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -38,7 +38,8 @@ export default function RootLayout({ children }) {
         </div>
         <div>
           <p>Facing Issues? <a>Get help</a></p>
-          <p>Suggestions for improvement? <a>Share feedback</a></p>
+          <p>Suggestions for improvement? <a href="https://mail.google.com/mail/?view=cm&to=sourarghyade@gmail.com&subject=Hello&body=I saw your website." target="_blank">Share feedback!</a>
+          </p>
           <div>
             <Link href={'/privacy'}>Privacy Policy</Link>
             <Link href={'/tos'}>Terms of Service</Link>
